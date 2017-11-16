a57f346c1dc9b36db133644557a15062f109b054
Marius Skaar LudVigsen <marius.ludvigsen@gmail.com>
2017-11-16 22:21:51 +0100

    Initial gh-pages

--- src/testrpc/fs.js
+++
@@ -1,9 +0,0 @@
-import * as Level from 'level-js';
-import * as levelup from 'levelup';
-import * as fs from 'level-filesystem';
-
-const leveldb = new Level('./database');
-
-console.log('LEVELJS: ', leveldb);
-const db = levelup('level-filesystem', { db: leveldb });
-module.exports = fs(db);
--- src/truffle/paths.js
+++
@@ -1,5 +0,0 @@
-export const CONTRACTS_PATH = '/contracts/';
-export const BUILD_PATH = '/build'
-export const BUILD_CONTRACTS_PATH = BUILD_PATH + CONTRACTS_PATH;
-export const MIGRATIONS_PATH = '/migrations/';
-export const TESTS_PATH = '/test/';
